6aa3df589c8da81712c605f98e265b2e1eefa9a7
akrm10 <akrm54391127@gmail.com>
2024-08-08 17:01:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+APP_ID = os.environ.get("21209802")
+APP_HASH = os.environ.get("eed1c8387c6ee80009527e07d9d58cc0")
+BOT_USERNAME = os.environ.get("Gmail_huntingbot")
 session = os.environ.get("TERMUX")
 SESSION = os.environ.get("TERMUX")
-token = os.environ.get("TOKEN")
+token = os.environ.get("6807629743:AAHsnFL_eb9F-egK2yjvvxVgKWIcQIhpNDA")
 fifthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 ispay = ['yes']
